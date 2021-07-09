00100e019a6edf2deb6b02eb78c6afd5527d11df
Petulla28 <60622928+petulla28@users.noreply.github.com>
2021-07-09 09:26:45 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:untitled1/authentication.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Power grid login',
+      home: Authentication(),
+    );
+  }
+}
+
+
+
+
